6b073dceb3b03b4d481f697efe3b16960493113c
FelixIuel <113988860+FelixIuel@users.noreply.github.com>
2026-04-21 22:36:41 +0200

    Ready for pilot
    Lets go boys

--- P8-IMMEXP26/Assets/Scripts/UI/MicIndicator.cs
+++ P8-IMMEXP26/Assets/Scripts/UI/MicIndicator.cs
@@ -3,12 +3,12 @@ using UnityEngine.UI;
 
 public class MicIndicator : MonoBehaviour
 {
-    public Image circle;
+    public Image image;
     private bool active = false;
 
     public void Toggle()
     {
         active = !active;
-        circle.color = active ? Color.green : Color.gray;
+        image.enabled = active;
     }
 }
